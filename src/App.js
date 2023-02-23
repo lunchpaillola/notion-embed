e7057939fd95625f0827cd36df5f2981d77911e0
Lola-Ojabowale <lola.ojabowale@gmail.com>
2023-02-23 18:22:14 -0500

    updates to package json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { useState, useEffect } from 'react';
-
-// We installed earlier. This will render content data fetched from the Notion.
 import { NotionRenderer } from 'react-notion';
-
-// For styling markdown content
 import 'react-notion/src/styles.css';
 import './App.css';
 
@@ -12,21 +8,14 @@ function App({ domElement }) {
   const pageID = domElement.getAttribute("data-pageid")
 
   useEffect(() => {
-    // notion-api-worker
-    fetch(
-      'https://notion-api.splitbee.io/v1/page/'+pageID
-    )
+    fetch(`https://notion-api.splitbee.io/v1/page/${pageID}`)
       .then((res) => res.json())
       .then((data) => setData(data));
-  }, []);
+  }, [pageID]);
 
   return (
     <div className="App">
-      <h1>
-        Notion Widget
-      </h1>
-
-      {/* Mount NotionRenderer and pass in data to render */}
+      <h1>Notion Widget</h1>
       <NotionRenderer blockMap={data} />
     </div>
   );
